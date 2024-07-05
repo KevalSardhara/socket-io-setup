48ba11bf0bd5e72177d590de8a00a2cbc661514c
keval_sardhara <keval.sardhara@caperminttechnologies.com>
2024-07-05 21:55:01 +0530

    add

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const http = require('http');
 const app = express();
 
+console.log("all changes merge");
+
 app.listen(3002, () => {
     console.log("run server port 5000");
 });
